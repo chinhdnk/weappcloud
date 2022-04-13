12b1961337607071c6e31b6ae69f71025aaccae9
chinhdnk <chinhdnk@gmail.com>
2022-04-13 15:17:26 +0700

    test

---
+++ webapp-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace webapp_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+}
+
